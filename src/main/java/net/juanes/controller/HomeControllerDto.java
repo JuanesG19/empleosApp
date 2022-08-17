a131ffd4027937262022ce46a9989b9fd1e99cc0
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2022-08-17 16:07:40 -0500

    Initialization

--- src/main/java/net/juanes/controller/HomeControllerDto.java
+++ src/main/java/net/juanes/controller/HomeControllerDto.java
@@ -1,13 +1,32 @@
 package net.juanes.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeControllerDto {
 
 	@GetMapping("/")
-	public String mostrarHome() {
+	public String mostrarHome(Model model) {
+		/**
+		model.addAttribute("mensaje", "Pagina Para Buscar Empleos");
+		model.addAttribute("fecha", new Date());
+		*/
+		
+		String nombre = "Auxiliar de Contabilidad";
+		Date fechaPub = new Date();
+		double salario = 90000;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fechaPub", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
+
+		
 		return "home";
 	}
 }
